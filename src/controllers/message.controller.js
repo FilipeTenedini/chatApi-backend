cbaa6e4a58f1d79254edaa44acafa1ac6b9e23d7
filipetenedini <tenedinifilipe@gmail.com>
2023-04-15 01:34:24 -0300

    (feat): implement post/sendMessage route

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,7 +1,34 @@
-function create(req, res) {
+import dayjs from 'dayjs';
+import participantService from '../services/participant.service.js';
+import messageService from '../services/message.service.js';
+import messageSchema from '../models/message.schema.js';
+
+async function create(req, res) {
   const { to, text, type } = req.body;
-  const { user } = req.header;
-  console.log(to, text, type, user, res);
+  const { user } = req.headers;
+
+  try {
+    if (messageSchema.validate({
+      to,
+      text,
+      type,
+      from: user,
+    }).error) return res.status(422).send('object infos error');
+
+    const participant = await participantService.findByName(user);
+    if (!participant) return res.status(422).send('participant not connected');
+
+    await messageService.create({
+      to,
+      text,
+      type,
+      from: user,
+      time: dayjs(Date.now()).format('HH:mm:ss'),
+    });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 export default { create };
--- src/services/participant.service.js
+++ src/services/participant.service.js
@@ -1,9 +1,7 @@
 import { db } from '../db/db.js';
 
 const findByName = (name) => db.collection('participants').findOne({ name });
-
 const create = (body) => db.collection('participants').insertOne(body);
-
 const findAll = () => db.collection('participants').find().toArray();
 
 export default { findByName, create, findAll };
